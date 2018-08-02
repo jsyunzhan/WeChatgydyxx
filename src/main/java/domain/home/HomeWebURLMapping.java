ceb5799240f2d2ec32a52c540cba1737211ce67b
e <yourmail@youremail.com.cn>
2018-08-02 13:46:46 +0800

    ghj

--- src/main/java/domain/home/HomeWebURLMapping.java
+++ src/main/java/domain/home/HomeWebURLMapping.java
@@ -7,44 +7,12 @@ public final class HomeWebURLMapping {
     public static final String HOME_ROOT = "/home/";
 
     /*******************通知管理*************************/
-    /**
-     * 去通知管理页面
-     */
-    public static final String NOTICE_MANAGEMENT_PAGE = HOME_ROOT + "noticemanpage";
-
-    /**
-     * 通知管理分页
-     */
-    public static final String NOTICE_MANAGEMENT_LIST = NOTICE_MANAGEMENT_PAGE + "/list";
-
-    /**
-     * 通知管理新增
-     */
-    public static final String NOTICE_MANAGEMENT_ADD = NOTICE_MANAGEMENT_PAGE + "/add";
-
-    /**
-     * 通知管理修改
-     */
-    public static final String NOTICE_MANAGEMENT_EDIT = NOTICE_MANAGEMENT_PAGE + "/edit";
-
-    /**
-     * 通知管理删除
-     */
-    public static final String NOTICE_MANAGEMENT_DELETE = NOTICE_MANAGEMENT_PAGE + "/delete/{id}";
-
-    /**
-     * 文件上传回显
-     */
-    public static final String NOTICE_MANAGEMENT_PICTURE_DETAIL = NOTICE_MANAGEMENT_PAGE + "/pictureDetail";
 
     /**
      * 根据文件路径显示
      */
     public static final String NOTICE_MANAGEMENT_PICTURE_SHOW = HOME_ROOT + "picture/show";
 
-    /**
-     * 通知管理图上传
-     */
-    public static final String NOTICE_MANAGEMENT_PICTURE_UPLOAD = NOTICE_MANAGEMENT_PAGE + "/pictureUpload/{name}";
+
 
 }
--- src/main/java/domain/home/controller/NoticeManagementController.java
+++ src/main/java/domain/home/controller/NoticeManagementController.java
@@ -7,11 +7,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
+
+import static domain.home.HomeWebURLMapping.NOTICE_MANAGEMENT_PICTURE_SHOW;
 
 @Controller
 @RequestMapping(value = "home/notice")
@@ -51,5 +57,35 @@ public class NoticeManagementController {
     @ResponseBody
     public NoticeEntity noticeDetails(@PathVariable("id") Long id){
         return noticeManagementService.noticeDetails(id);
+    }
+
+    /**
+     * 根据图片地址查询图片
+     * @param noticeEntity
+     * @return
+     * @throws IOException
+     */
+    @RequestMapping(value = NOTICE_MANAGEMENT_PICTURE_SHOW)
+    @ResponseBody
+    public String[] getPictureByte(@RequestBody NoticeEntity noticeEntity) throws IOException {
+
+        //获取图片路径
+        final String path = noticeEntity.getPicturePath();
+        //获取图片路径地址
+        final String[] pathArr=path.split(",");
+        //需要返回的base64数组
+        String[] base64Array = new String[pathArr.length];
+
+        for (int i=0;i<pathArr.length;i++){
+            //图片地址
+            String pahtStr = pathArr[i];
+            //获取数组
+            byte[] imageByte = Files.readAllBytes(Paths.get(pahtStr));
+            //转码
+            String base64String= java.util.Base64.getEncoder().encodeToString(imageByte);
+            //添加到64数组
+            base64Array[i] = base64String;
+        }
+        return base64Array;
     }
 }
