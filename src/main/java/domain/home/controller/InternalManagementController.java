cdbdfca7fc4ee859fc20ce0bc24844b7075c28c5
e <yourmail@youremail.com.cn>
2018-08-06 14:05:57 +0800

    ghj

--- src/main/java/domain/home/controller/InternalManagementController.java
+++ src/main/java/domain/home/controller/InternalManagementController.java
@@ -2,23 +2,30 @@ package domain.home.controller;
 
 import domain.home.entity.NoticeEntity;
 import domain.home.service.InternalManagementService;
+import domain.home.service.NoticeManagementService;
 import domain.shiro.controller.AbstractActionController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping(value = "home/internal")
 public class InternalManagementController extends AbstractActionController{
 
     final private InternalManagementService internalManagementService;
+    final private NoticeManagementService noticeManagementService;
 
     @Autowired
-    public InternalManagementController(InternalManagementService internalManagementService){
+    public InternalManagementController(InternalManagementService internalManagementService,NoticeManagementService noticeManagementService){
         this.internalManagementService = internalManagementService;
+        this.noticeManagementService = noticeManagementService;
     }
 
 
@@ -35,5 +42,15 @@ public class InternalManagementController extends AbstractActionController{
         return internalManagementService.internalList(noticeEntity);
     }
 
-
+    @RequestMapping(value = "/details/{id}")
+    @ResponseBody
+    public ModelAndView internalDetails(@PathVariable("id") Long id){
+        final NoticeEntity noticeEntity = noticeManagementService.noticeDetails(id);
+        final Map<String, Object> map = new HashMap<>(4);
+        map.put("title",noticeEntity.getTitle());
+        map.put("details",noticeEntity.getDetails());
+        map.put("picturePath",noticeEntity.getPicturePath());
+        map.put("createDate",noticeEntity.getCreateDate());
+        return new ModelAndView("details",map);
+    }
 }
